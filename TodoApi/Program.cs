f1a9dbe50a31eaa73e1b28575c484d4f868724b2
Marcospantunes <marcospantunes203@gmail.com>
2024-11-23 12:43:13 -0300

    reafactor: colocado o MapGroup para facilitar ao escrever endpoints do todoitems

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -17,35 +17,37 @@ if(app.Environment.IsDevelopment()) {
   app.UseOpenApi();
   app.UseSwaggerUi(config => {
     config.DocumentTitle = "TodoAPI";
-    config.Path = "/swagger";
+    config.Path = "/";
     config.DocumentPath = "/swagger/{documentName}/swagger.json";
     config.DocExpansion = "list";
   });
 }
 
-app.MapGet("/todoitems", async (TodoDb db) => 
+var todoItems = app.MapGroup("/todoitems");
+
+todoItems.MapGet("/", async (TodoDb db) => 
   await db.Todos .ToListAsync()
 );
 
-app.MapGet("/todoitems/complete", async (TodoDb db) => 
+todoItems.MapGet("/complete", async (TodoDb db) => 
   await db.Todos.Where(t => t.isComplete).ToListAsync()
 );
 
-app.MapGet("/todoitems/{id}", async (int id, TodoDb db) =>
+todoItems.MapGet("/{id}", async (int id, TodoDb db) =>
   await db.Todos.FindAsync(id)
     is Todo todo
       ? Results.Ok(todo)
       : Results.NotFound()
 );
 
-app.MapPost("/todoitems", async (Todo todo, TodoDb db) => {
+todoItems.MapPost("", async (Todo todo, TodoDb db) => {
   db.Todos.Add(todo);
   await db.SaveChangesAsync();
   
   return Results.Created($"/todoitems/{todo.Id}", todo);
 });
 
-app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) => {
+todoItems.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) => {
   var todo = await db.Todos.FindAsync(id);
 
   if(todo is null) return Results.NotFound();
@@ -58,7 +60,7 @@ app.MapPut("/todoitems/{id}", async (int id, Todo inputTodo, TodoDb db) => {
   return Results.NoContent();
 });
 
-app.MapDelete("/todoitems/{id}", async (int id, TodoDb db) => {
+todoItems.MapDelete("/todoitems/{id}", async (int id, TodoDb db) => {
   if(await db.Todos.FindAsync(id) is Todo todo) {
     db.Todos.Remove(todo);
     await db.SaveChangesAsync();
